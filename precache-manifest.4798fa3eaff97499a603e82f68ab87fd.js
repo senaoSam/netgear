1356910c6e541053428f0a486e5d518e5eb4982d
莊逸軒 <sam.chuang@senao.com>
2024-02-05 16:02:15 +0800

    first blood

---
+++ precache-manifest.4798fa3eaff97499a603e82f68ab87fd.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297d4f94f19dbc1050431037a528226a",
+    "url": "'/netgear/index.html"
+  },
+  {
+    "revision": "0f71e48ef127624f418a",
+    "url": "'/netgear/static/css/2.90579cf4.chunk.css"
+  },
+  {
+    "revision": "fb0bafc18c0005baa340",
+    "url": "'/netgear/static/css/main.8c3b5c52.chunk.css"
+  },
+  {
+    "revision": "0f71e48ef127624f418a",
+    "url": "'/netgear/static/js/2.e8a91567.chunk.js"
+  },
+  {
+    "revision": "ee06d740c1a3de9bae2af303199775c9",
+    "url": "'/netgear/static/js/2.e8a91567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0bafc18c0005baa340",
+    "url": "'/netgear/static/js/main.c8fce793.chunk.js"
+  },
+  {
+    "revision": "8aaff6fad005d7cfce59",
+    "url": "'/netgear/static/js/runtime-main.62a68916.js"
+  },
+  {
+    "revision": "60d891473e173031cb0d074ad161bf4f",
+    "url": "'/netgear/static/media/angle-up.60d89147.svg"
+  },
+  {
+    "revision": "f0a0e582ccb2b4ee5c8163c61e16358a",
+    "url": "'/netgear/static/media/errorboundary.f0a0e582.svg"
+  },
+  {
+    "revision": "94895ab83f220ae92544ac26d9fd9352",
+    "url": "'/netgear/static/media/iconetgear.94895ab8.woff"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "'/netgear/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "'/netgear/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "'/netgear/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "'/netgear/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "'/netgear/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "'/netgear/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "'/netgear/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "'/netgear/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "'/netgear/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "'/netgear/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "'/netgear/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "'/netgear/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "'/netgear/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "'/netgear/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "'/netgear/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "'/netgear/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "'/netgear/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "'/netgear/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "'/netgear/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "'/netgear/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "'/netgear/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "'/netgear/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "'/netgear/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "'/netgear/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "5a8fa2f66ae6a990cd78b2dfab10d4f1",
+    "url": "'/netgear/static/media/switch_logo.5a8fa2f6.svg"
+  },
+  {
+    "revision": "3908d63ebbc74ae268763cccdd0c04f9",
+    "url": "'/netgear/static/media/switch_logo_login.3908d63e.svg"
+  }
+]);
